05bd3a1158af6b176618341654c6ba8303e779a5
Alessandro B. Cesta <54996931+Alessandro1918@users.noreply.github.com>
2021-12-07 01:41:13 -0300

    Add files via upload

--- src/db/indexDev.js
+++ src/db/indexDev.js
@@ -3,11 +3,11 @@ const { Pool } = require('pg')
 let pool;
 if (process.env.NODE_ENV === 'dev') {
   pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    user: process.env.DB_USER_DEV,
+    host: process.env.DB_HOST_DEV,
+    database: process.env.DB_NAME_DEV,
+    password: process.env.DB_PASSWORD_DEV,
+    port: process.env.DB_PORT_DEV,
   })
 } else {
   pool = new Pool({
@@ -23,7 +23,7 @@ if (process.env.NODE_ENV === 'dev') {
 pool
   .connect()
   .then(client => {
-    console.log('DB connected!')
+    console.log('DB \'marketplace\' connected!')
     client.release();
   })
   .catch(err => console.log(err))
